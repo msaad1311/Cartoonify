aec17ebb57811df97ac31fdcfc3861a3e3d606d6
Saad-Muhammad <Saad.Muhammad@bcg.com>
2022-11-15 15:00:03 -0500

    added the web cartoonization

--- src/image_reader.py
+++ src/image_reader.py
@@ -1,17 +1,16 @@
+# from pyodide.http import pyfetch
+# from js import document
+# import asyncio
+# from io import BytesIO
 import matplotlib.pyplot as plt
-import numpy as np
+# import imageio.v3 as iio
+import io, base64
 import cv2
 
-image = cv2.imread('chair_man.jpg')
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-row, col = 1, 2
-fig, axs = plt.subplots(row, col, figsize=(15, 10))
-fig.tight_layout()
+img = cv2.imread("./chair_man.jpg")
+gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# print(img.shape)
 
-axs[0].imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-axs[0].set_title('Original')
-
-axs[1].imshow(cv2.cvtColor(gray_image, cv2.COLOR_BGR2RGB))
-axs[1].set_title('Grayscale')
-
-fig.show()
+plt.imshow(img)
+plt.savefig('image.png',dpi=100)
+# plt.show()
